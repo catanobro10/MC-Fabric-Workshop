582299bbf1d2caeeb51f76b921ee713e95e6d3d0
Giancarlo Garcia Deleon <44848977+ggiande@users.noreply.github.com>
2022-01-20 19:17:49 -0800

    Workshop init (#2)
    * Created the workshop template
    
    * Added the ItemInit class to ensure git tracking. Mixing package neglected.

---
+++ src/main/java/net/workshop/init/ItemInit.java
@@ -0,0 +1,11 @@
+package net.workshop.init;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
+import net.workshop.main;
+
+public class ItemInit {
+
+}
